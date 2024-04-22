92e3e9e78c005300e3cc5df809c0e3227083930c
minhdq-98 <minhdao998@gmail.com>
2024-04-22 16:17:07 +0700

    init placce holder for algorithm

--- src/SecretKey.java
+++ src/SecretKey.java
@@ -1,35 +1,90 @@
-import src
+class SecretKeyGuesser {
+    private String initSecretKey = "RRRRRRRRRMITRMIT";
+    
+    public void start() {
+        SecretKey secret = new SecretKey(initSecretKey);
+        String allRString = "R".repeat(16);
+        String allMString = "M".repeat(16);
+        String allIString = "I".repeat(16);
+        String allTString = "T".repeat(16);
+        String correctGuess = allRString;
 
+        // Total number of R
+        int rAccuracy = secret.guess(allRString);
+        if (rAccuracy == 16) {
+            System.out.println("Secret key: " + allRString);
+            return;
+        }
+        // Total number of M
+        int mAccuracy = secret.guess(allMString);
+        if (mAccuracy == 16) {
+            System.out.println("Secret key: " + allMString);
+            return;
+        }
+        // Total number of I
+        int iAccuracy = secret.guess(allIString);
+        if (iAccuracy == 16) {
+            System.out.println("Secret key: " + allIString);
+            return;
+        }
+        // Total number of T
+        int tAccuracy = 16 - rAccuracy - mAccuracy - iAccuracy;
+        if (tAccuracy == 16) {
+            System.out.println("Secret key: " + allTString);
+            return;
+        }
+
+        int mostAccuracy = rAccuracy;
+
+        if (mAccuracy > mostAccuracy) {
+            mostAccuracy = mAccuracy;
+            correctGuess = allMString;
+        }
+        if (iAccuracy > mostAccuracy) {
+            mostAccuracy = iAccuracy;
+            correctGuess = allIString;
+        }
+        if (tAccuracy > mostAccuracy) {
+            mostAccuracy = tAccuracy;
+            correctGuess = allTString;
+        }
+        return;
+    }
+}
 public class SecretKey {
     private String correctKey;
     private int counter;
 
-    public SecretKey() {
-    // for the real test, your program will not know this
-    correctKey = "RRRRRRRRRMITRMIT";
-    counter = 0;
+    public SecretKey(String inputCorrectKey) {
+        // for the real test, your program will not know this
+        correctKey = inputCorrectKey;
+        counter = 0;
     }
 
     public int guess(String guessedKey) {
-    counter++;
-    // validation
-    if (guessedKey.length() != correctKey.length()) {
-        return -1;
-    }
-    int matched = 0;
-    for (int i = 0; i < guessedKey.length(); i++) {
-        char c = guessedKey.charAt(i);
-        if (c != 'R' && c != 'M' && c != 'I' && c != 'T') {
-        return -1;
-    }
-    if (c == correctKey.charAt(i)) {
-        matched++;
-    }
-    }
-    if (matched == correctKey.length()) {
-        System.out.println("Number of guesses: " + counter);
-    }
-    return matched;
+        if (counter >= 16) {
+            System.out.println("Number of guesses: " + counter);
+            return -1;
+        }
+        counter++;
+        // validation
+        if (guessedKey.length() != correctKey.length()) {
+            return -1;
+        }
+        int matched = 0;
+        for (int i = 0; i < guessedKey.length(); i++) {
+            char c = guessedKey.charAt(i);
+            if (c != 'R' && c != 'M' && c != 'I' && c != 'T') {
+                return -1;
+            }
+            if (c == correctKey.charAt(i)) {
+                matched++;
+            }
+        }
+        if (matched == correctKey.length()) {
+            System.out.println("Number of guesses: " + counter);
+        }
+        return matched;
     }
 
     public static void main(String[] args) {
---
+++ src/test.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class test {
+
+    public static void main(String[] args) {
+        int incorrectGuesses = 5;
+        // Check if first run
+        boolean iterated = false;
+        String temp = "";
+        System.out.println("Welcome to the guessing game! Guess one letter "
+                + "at a time. You have " + incorrectGuesses + " incorrect "
+                        + "guesses remaining.");
+
+        Scanner reader = new Scanner(System.in);
+        
+        String word = "sleepy";
+        String dashedWord = createDashedWord(word);
+        System.out.println("The Secret Word Is: " + dashedWord + 
+                " (" + word.length() + ")");
+        
+        do {
+            System.out.print("Your Guess: ");
+            String guess = reader.nextLine();
+            boolean correctGuess = false;
+
+            for (int i = 0; i < word.length(); i++) {
+                if (guess.equals(Character.toString(word.charAt(i)))) {
+                    correctGuess = true;
+                    if (!iterated)
+                        temp += Character.toString(word.charAt(i));
+                    else {
+                        String holder = Character.toString(
+                                temp.charAt(i)).replace("-", guess);
+                        temp = temp.substring(0, i) + holder + 
+                                temp.substring(i + 1, temp.length());
+                    }
+                } else {
+                    // Add '-' for first run through (1st guess incorrect)
+                    if (!iterated) {
+                        temp += "-";
+                    }
+                }
+            }
+            iterated = true;
+            
+            if (correctGuess) {
+                System.out.println("The word contains the letter " + guess + ".");
+            } else {
+                System.out.println("The word does not contain the letter " + guess + ".");
+                incorrectGuesses--;
+                System.out.println("You have " + incorrectGuesses + 
+                        " incorrect guesses remaining.");
+            }
+            
+            System.out.println(temp);
+            if (temp.equals(word)) {
+                System.out.println("You guessed correctly!");
+                break;
+            }
+        
+        } while (incorrectGuesses > 0);
+        
+        if (incorrectGuesses <= 0) {
+            System.out.println("Sorry, you didn't guess the secret word \"" + word + "\".");
+        }
+        
+        reader.close();
+
+    }
+    
+    private static String createDashedWord(String word) {
+        String output = "";
+        for (int i = 0; i < word.length(); i++) {
+            output += "-";
+        }
+        return output;
+    }
+
+}
